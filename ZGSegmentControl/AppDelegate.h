23b8ec7d14080363cddd955a4bf26ce9f2913494
zhangzhi0903 <18310127735@163.com>
2018-05-15 17:43:31 +0800

    Initial Commit

---
+++ ZGSegmentControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZGSegmentControl
+//
+//  Created by 张传志 on 2018/5/15.
+//  Copyright © 2018年 offcn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZGSegmentControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZGSegmentControl
+//
+//  Created by 张传志 on 2018/5/15.
+//  Copyright © 2018年 offcn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
